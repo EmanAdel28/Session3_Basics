93ef534a11420f67b9fc6e9f1f1e74b0b56b61c6
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-13 16:17:19 +0200

    1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no

--- Demo_Session3_C#_Basics/Program.cs
+++ Demo_Session3_C#_Basics/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
+
+            Console.WriteLine("Enter Number :");
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number % 3 == 0 && Number % 4 == 0)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+            #endregion
         }
     }
 }
