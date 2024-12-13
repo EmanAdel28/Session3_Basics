656deae17e29e3a8254186baaa61f3aacea972ba
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-13 16:31:55 +0200

    2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.

--- Demo_Session3_C#_Basics/Program.cs
+++ Demo_Session3_C#_Basics/Program.cs
@@ -6,13 +6,25 @@
         {
             #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
 
-            Console.WriteLine("Enter Number :");
-            int Number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Number :");
+            //int Number = int.Parse(Console.ReadLine());
 
-            if (Number % 3 == 0 && Number % 4 == 0)
-                Console.WriteLine("Yes");
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+
+            #endregion
+
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            Console.WriteLine("Enter Number :");
+            int Num = int.Parse(Console.ReadLine());
+
+            if (Num < 0)
+                Console.WriteLine("It Is Negative Number");
             else
-                Console.WriteLine("No");
+                Console.WriteLine("It Is Positive Number");
+
 
             #endregion
         }
