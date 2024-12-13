798f9b8ffd44a54246587a9b8bdb0d6af0deac88
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-13 13:47:09 +0200

    Explicit Casting

--- Demo_Session3_C#_Basics/Program.cs
+++ Demo_Session3_C#_Basics/Program.cs
@@ -6,9 +6,19 @@
         {
             #region Value Type Casting
             #region Implicit Casting - Safe Casting
-            int X = 4;
-            long Y = X;
+            //int X = 4;
+            //long Y = X;
 
+            #endregion
+
+            #region Explicit Casting - UnSafe Casting
+            long X = 101010101010;
+            int Y;
+            checked
+            {
+                Y = (int)X;
+            }
+            Console.WriteLine(Y);
             #endregion
 
             #endregion
